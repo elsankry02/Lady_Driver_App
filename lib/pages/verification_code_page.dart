6a7a1a3de7ef426b0ab072a57803490ee0fc00bb
elsankary-02 <githup800900@gmail.com>
2025-01-02 10:11:41 +0200

    create VerificationCodePage

---
+++ lib/pages/verification_code_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:lady_driver/core/constant/color_manger.dart';
+
+class VerificationCodePage extends StatelessWidget {
+  const VerificationCodePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        children: [
+          Text(
+              textAlign: TextAlign.center,
+              'ادخل الرمز الذي تم إرساله إلي عنولن البريد \nالالكترونى',
+              style:
+                  textTheme.titleMedium!.copyWith(fontWeight: FontWeight.w500)),
+          const SizedBox(height: 7),
+          Text(
+            textAlign: TextAlign.center,
+            'be*****@gmail.com',
+            style: textTheme.titleMedium!.copyWith(
+                fontWeight: FontWeight.w500, color: ColorManger.kPrimaryColor),
+          ),
+        ],
+      ),
+    );
+  }
+}
